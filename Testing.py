f584ff1af7cc392ac01a89649be55bf0645bf400
alper <alper.dom@sbgenomics.com>
2019-10-27 23:38:58 +0300

    Testing serial

--- Testing.py
+++ Testing.py
@@ -8,6 +8,14 @@ ser.baudrate = 9600
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
 line_break = False
+def print_commands():
+    my_str = ""
+    my_str += "0. Get data\n"
+    my_str += "1. Turn on the heater\n"
+    my_str += "2. Turn off the heater\n"
+    my_str += "3. Get heater status\n"
+    my_str += "4. Test the connection to the heater\n"
+    print(my_str)
 try:
     # wait for setup phase
     while True:
@@ -24,6 +32,13 @@ try:
             else:
                 line_break = False
         print('Break out of while')
+        while True:
+            text = -1
+            while text not in {'0', '1', '2', '3', '4'}:
+                print_commands()
+                text = input("Select a command: ")
+            ser.write(str.encode(text))
+            time.sleep(2)
 except Exception as e:
     print(e)
 finally:
