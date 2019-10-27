b6de7d27fae4519107e878b9c991a551a6d20d0c
alper <alper.dom@sbgenomics.com>
2019-10-27 23:29:57 +0300

    Testing serial read

---
+++ Testing.py
@@ -0,0 +1,30 @@
+import serial
+import RPi.GPIO as GPIO
+import time
+
+ser = serial.Serial("/dev/ttyUSB0", 9600)  # change ACM number as found from ls /dev/tty/ACM*
+ser.baudrate = 9600
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+line_break = False
+try:
+    # wait for setup phase
+    while True:
+        read_ser = ser.readline().decode().strip()
+        print(read_ser)
+        print('{}'.format(read_ser))
+        if '4. Test the connection' in read_ser:
+            time.sleep(2)
+            break
+        elif line_break:
+            break
+        elif read_ser == '\n':
+            line_break = True
+        else:
+            line_break = False
+    print('Break out of while')
+except Exception as e:
+    print(e)
+finally:
+    GPIO.cleanup()
