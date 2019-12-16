60418c6fdcc89796ff6869d1d22e74148db7a347
Alper <h.alper.dom@gmail.com>
2019-12-16 22:18:28 +0300

    fixed log rotate

--- log_rotate.sh
+++ log_rotate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tar -zcvf "/home/pi/smart_thermostat/logs/arduino-log-$(date +"%Y-%m-%d").tar.gz" /home/pi/smart_thermostat/arduino.log
+tar -zcvf "/home/pi/logs/arduino-$(date +"%Y-%m-%d").tar.gz" /home/pi/smart_thermostat/arduino.log
