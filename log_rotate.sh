2435011a070a2037508d3cf231d7c16556282650
alper <alper.dom@sbgenomics.com>
2020-01-07 23:42:12 +0300

    updated log rotate to delete log file

--- log_rotate.sh
+++ log_rotate.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-tar -zcvf "/home/pi/logs/arduino-$(date +"%Y-%m-%d").tar.gz" /home/pi/smart_thermostat/arduino.log
+tar -zcvf "/home/pi/logs/arduino-$(date +"%Y-%m-%d").tar.gz" /home/pi/smart_thermostat/arduino.log && rm /home/pi/smart_thermostat/arduino.log
+
+# crontabs
+# root
+# 1 0 * * * . /home/pi/smart_thermostat/log_rotate.sh && /sbin/shutdown -r now
+
+# pi
+# @reboot cd /home/pi/smart_thermostat && nohup python3 main.py &
